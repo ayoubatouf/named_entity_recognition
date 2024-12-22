4df25b40ce988e6ba648158a467c0a05df5cf62d
ayoubatouf <atouf.ayoub1@gmail.com>
2024-12-22 16:04:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,1 @@
-from training.trainer import NERModelTrainer
 
-
-if __name__ == "__main__":
-    file_path = "/content/drive/MyDrive/NER/ner_dataset.csv"
-    save_path = "/content/drive/MyDrive"
-
-    ner_trainer = NERModelTrainer(file_path)
-    ner_trainer.train_model(epochs=5)
-    ner_trainer.model.save(save_path)
-
-    test_results = ner_trainer.evaluate_model()
-    print("Test Results: ", test_results)
